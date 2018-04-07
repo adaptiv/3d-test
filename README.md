1099f5efe1a078e3a8b7eb6bc71948bd81ef08a7
Johan Dewe <johan@dewe.net>
2018-04-07 22:08:10 +0200

    first attempt

---
+++ README.md
@@ -0,0 +1,21 @@
+# 3d-test
+Exploring js 3D engines
+
+## Resources
+* [three.js](https://threejs.org/docs/)
+* three.js video tutorial: https://www.youtube.com/watch?v=ABV1mK1CGOY
+* three.js 101: https://medium.com/@necsoft/three-js-101-hello-world-part-1-443207b1ebe1
+* 3d file formats: https://all3dp.com/3d-file-format-3d-files-3d-printer-3d-cad-vrml-stl-obj/
+
+* JavaScript Performance Monitor https://github.com/mrdoob/stats.js/
+* GUI http://workshop.chromeexperiments.com/examples/gui/#1--Basic-Usage
+* OrbitControls https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/controls/OrbitControls.js
+
+
+https://github.com/project-codeology/codeology
+
+## Notes
+
+* Investigate file formats for content delivery
+  * [ ] `obj` + `mtl`
+  * [ ] `gltf`
---
+++ three-1/app.js
@@ -0,0 +1,38 @@
+// ------------------------------------------------
+// BASIC SETUP
+// ------------------------------------------------
+
+// renderer
+var renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setClearColor("#000000");
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// empty scene
+var scene = new THREE.Scene();
+
+// a basic perspective camera
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 4;
+
+// Cube Mesh with basic material
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({color: "#433F81"});
+var cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+// Render Loop
+var render = function() {
+  requestAnimationFrame(render);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  // Render the scene
+  renderer.render(scene, camera);
+};
+
+render();
