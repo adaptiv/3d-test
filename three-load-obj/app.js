c0b4d715e1066862f91850620481c2ee1578517a
Johan Dewe <johan@dewe.net>
2018-04-13 19:39:02 +0200

    load w/o mtrl

--- three-load-obj/app.js
+++ three-load-obj/app.js
@@ -1,91 +1,80 @@
-var container, stats;
+var container, controls;
 var camera, scene, renderer;
-var mouseX = 0, mouseY = 0;
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
+var mesh;
+
+var modelPath = 'model/';
 
 init();
-animate();
+render();
 
 function init() {
+  // dom
   container = document.createElement('div');
   document.body.appendChild(container);
 
+  // camera
   camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-  camera.position.z = 250;
+  camera.position.z = 50;
+  camera.position.y = 20
 
   // scene
   scene = new THREE.Scene();
-  var ambientLight = new THREE.AmbientLight(0xffffff, 200);
+  scene.add(camera);
+
+  // lights
+  var ambientLight = new THREE.AmbientLight(0xcccccc, 2000);
   scene.add(ambientLight);
 
   var pointLight = new THREE.PointLight(0xffffff, 10);
   camera.add(pointLight);
 
-  scene.add(camera);
-
   // model
-  var onProgress = function(xhr) {
-    if (xhr.lengthComputable) {
-      var percentComplete = xhr.loaded / xhr.total * 100;
-      console.log(Math.round(percentComplete, 2) + '% downloaded');
-    }
-  };
-
-  var onError = function(xhr) {};
-
-  THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
-
   var mtlLoader = new THREE.MTLLoader();
-  mtlLoader.setPath('../farmhouse_model/');
-
+  mtlLoader.setPath(modelPath);
   mtlLoader.load('FarmHouse.mtl', function(materials) {
-    var objLoader = new THREE.OBJLoader();
 
     materials.preload();
-    console.log(materials);
 
+    var objLoader = new THREE.OBJLoader();
     objLoader.setMaterials(materials);
-    objLoader.setPath('../farmhouse_model/');
-
-    objLoader.load('FarmHouse.obj', function(object) {
-      object.position.y = -95;
-      scene.add(object);
-    }, onProgress, onError);
+    objLoader.setPath('model/');
+    objLoader.load(
+      'FarmHouse.obj',
+      function(object) {
+        mesh = object;
+        scene.add(mesh)
+      },
+      progressLogger,
+      console.log
+    );
   });
 
   //
-  renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer({antialias: true});
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   container.appendChild(renderer.domElement);
-  document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
   window.addEventListener('resize', onWindowResize, false);
 }
 
+// render loop
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
 function onWindowResize() {
-  windowHalfX = window.innerWidth / 2;
-  windowHalfY = window.innerHeight / 2;
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function onDocumentMouseMove(event) {
-  mouseX = (event.clientX - windowHalfX) / 2;
-  mouseY = (event.clientY - windowHalfY) / 2;
-}
-
-//
-function animate() {
-  requestAnimationFrame(animate);
-  render();
-}
-
-function render() {
-  camera.position.x += (mouseX - camera.position.x) * .05;
-  camera.position.y += (-mouseY - camera.position.y) * .05;
-  camera.lookAt(scene.position);
-  renderer.render(scene, camera);
-}
+function progressLogger(xhr) {
+  if (xhr.lengthComputable) {
+    var percentComplete = xhr.loaded / xhr.total * 100;
+    console.log(Math.round(percentComplete, 2) + '% downloaded');
+  }
+};
